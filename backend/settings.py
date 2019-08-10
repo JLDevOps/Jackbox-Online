620dc0f9e4cdfc8b7d2f386de9c976b387e8b889
Jimmy <jldevops@gmail.com>
2019-08-10 11:19:51 -0400

    Added python white noise settings

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
